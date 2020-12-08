302c468a53ba8d03451b79a05313247786dcda33
hard-learningboy <1049281493@qq.com>
2020-12-09 00:02:46 +0800

    eighth change

--- mall-learning/mall-tiny-04/src/main/java/com/mcx/mall/tiny/common/utils/JwtTokenUtil.java
+++ mall-learning/mall-tiny-04/src/main/java/com/mcx/mall/tiny/common/utils/JwtTokenUtil.java
@@ -1,8 +1,13 @@
 package com.mcx.mall.tiny.common.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Date;
 import java.util.Map;
@@ -20,6 +25,7 @@ import java.util.Map;
  * @date 2020/12/8 16:58
  */
 public class JwtTokenUtil {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtTokenUtil.class);
     @Value("${jwt.secret}")
     private String secret;
     @Value("${jwt.expiration}")
@@ -46,5 +52,71 @@ public class JwtTokenUtil {
         return new Date(System.currentTimeMillis() + expiration * 1000);
     }
 
+    /**
+     * 从token中获取payload（有效负载）
+     * @param token
+     * @return Claims
+     */
+    private Claims getClaimsFromToken(String token){
+        Claims claims = null;
+        try{
+            claims = Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        }catch (Exception e){
+            LOGGER.info("JWT格式验证失败:{}",token);
+        }
+
+        return claims;
+    }
+
+    /**
+     * 从token中获取用户名
+     * @param token
+     * @return String
+     */
+    public String getUserNameFromToken(String token){
+         String username = null;
+
+        try{
+            Claims claims = getClaimsFromToken(token);
+            username = claims.getSubject();
+        }catch (Exception e){
+            LOGGER.info("获取用户名失败:{}",token);
+        }
+        return username;
+    }
+
+    /**
+     * 验证token是否还有效
+     * @param token
+     * @param userDetails
+     * @return boolean
+     */
+    public boolean validateToken(String token , UserDetails userDetails){
+        String username = getUserNameFromToken(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
+
+    /**
+     * 判断token是否已经失效
+     * @param token
+     * @return boolean
+     */
+    private boolean isTokenExpired(String token){
+        Date expiredDate = getExpiredDateFromToken(token);
+        return expiredDate.before(new Date());
+    }
+
+    /**
+     * 从token中获取过期时间
+     * @param token
+     * @return Date
+     */
+    private Date getExpiredDateFromToken(String token){
+        Claims claims = getClaimsFromToken(token);
+        return claims.getExpiration();
+    }
 
 }
